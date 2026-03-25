55bcea5ad85d90815cfb1187a382de529cc437b5
Simon Norris <snorris2@learn.athabascau.ca>
2026-03-25 01:18:21 -0600

    lastUpdate.js

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
     <li><s>Build site navigation bar</s></li>
     <li><b>fix site navigation bar on mobile pages</b></li>
     <li><s>Build site footer</s></li>
+    <li><s>Add javascript to update lastupdate automatically</s></li>
     <li>Add basic resume</li>
     <li>Add basic portfolio</li>
     <li>Build basic contact form</li>
---
+++ scripts/updateDate.js
@@ -0,0 +1,23 @@
+// function to find the 'last updated' element and inject the last modified 
+// date from the webpage document
+const updateLastUpdated = (elementId) => {
+  const targetElement = document.getElementById(elementId);
+
+  if (targetElement) {
+    // document.lastModified returns a string
+    const lastUpdated = new Date(document.lastModified);
+    // formatting:
+    const formattedDate = lastUpdated.toLocaleDateString('en-CA', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+
+    targetElement.textContent = formattedDate;
+  }
+};
+
+// run the function once HTML is loaded
+document.addEventListener('DOMContentLoaded', () => {
+  updateLastUpdated('updateId');
+});
